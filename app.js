13620fac63425d5a6ff7d736600ba9540a1988d7
Subramaniam Jeyaseelan <jeyaseelan421@gmail.com>
2022-04-01 18:23:17 +0530

    MCq question tried

---
+++ app.js
@@ -0,0 +1,12 @@
+// var a = ""
+//     if(a){
+//         console.log("hello");
+//     }
+
+    var a ="";
+    if(a){
+        console.log('yes');
+    }
+    else{
+        console.log('success');
+    }
